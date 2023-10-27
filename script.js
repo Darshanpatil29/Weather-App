9bd7e4bac9b924b9571c0ce94000ad8af6645719
Darshanpatil29 <113468781+Darshanpatil29@users.noreply.github.com>
2023-10-27 20:30:10 +0530

    script.js

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
 let weather = {
     apiKey: "7770e2d0961df6d6f7707ea49e90a890",
-    fetchWeather: async function (city) {
-      const response= await fetch(
+    fetchWeather: async function (city,lat,long) {
+        if(city){
+      var response= await fetch(
         "https://api.openweathermap.org/data/2.5/weather?q=" +
           city +
           "&units=metric&appid=" +
           this.apiKey
       )
+        }
+        else{
+            var response= await fetch(
+                "https://api.openweathermap.org/data/2.5/weather?lat=" +
+                  lat +
+                  "&lon=" + long +
+                  "&units=metric&appid=" +
+                  this.apiKey
+              )
+              this.backgroundImage();
+        }
     const result= await response.json();
     console.log(result);
     this.displayWeather(result);
+    },
+    backgroundImage: function(){
+      document.body.style.backgroundImage =
+      "url('https://source.unsplash.com/random/nature')";
     },
     displayWeather: function (data) {
       const { name } = data;
@@ -27,17 +43,33 @@ let weather = {
         "Wind speed: " + speed + " km/h";
       document.querySelector(".weather").classList.remove("loading");
       document.body.style.backgroundImage =
-        "url('https://source.unsplash.com/1600x900/?" + name + "')";
+        "url('https://source.unsplash.com/random/nature')";
     },
     search: function () {
-      this.fetchWeather(document.querySelector(".search-bar").value);
-    },
+        const city = document.querySelector(".search-bar input[name='search']").value;
+        this.fetchWeather(city);
+      },
   };
   
-  document.querySelector(".search button").addEventListener("click", function () {
+//   document.querySelector(".search button").addEventListener("click", function () {
+//     weather.search();
+//   });
+document.querySelector(".search button").addEventListener("click", function () {
     weather.search();
   });
   
+  document.querySelector(".search-bar").addEventListener("submit", function (event) {
+    event.preventDefault();
+    weather.search();
+  });
+  
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(async position => {
+        long = position.coords.longitude; // Use longitude for 'long' and latitude for 'lat'
+        lat = position.coords.latitude;
+        weather.fetchWeather(null,lat,long);
+    });
+}
   document
     .querySelector(".search-bar")
     .addEventListener("keyup", function (event) {
@@ -45,5 +77,3 @@ let weather = {
         weather.search();
       }
     });
-  
-  weather.fetchWeather("Pune");
